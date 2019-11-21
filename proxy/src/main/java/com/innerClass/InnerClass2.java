d92a71b2e0912c0ad9bc22d56bf5e90a61601a5c
Song-Fang <sof10@pitt.edu>
2019-11-20 20:53:26 -0500

    Signed-off-by: Song-Fang <sof10@pitt.edu>

--- proxy/src/main/java/com/innerClass/InnerClass2.java
+++ proxy/src/main/java/com/innerClass/InnerClass2.java
@@ -1,4 +1,16 @@
 package com.innerClass;
 
 public class InnerClass2 {
+    public static void main(String[] args) {
+
+    }
+
+    public Comparable getCompare(){
+        class MyComparable implements Comparable{
+            public int compareTo(Object o) {
+                return 0;
+            }
+        }
+        return new MyComparable();
+    }
 }
