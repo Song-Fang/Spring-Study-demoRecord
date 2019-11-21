02b71d6393c1c2a402099b9751e6853ae4280363
Song-Fang <sof10@pitt.edu>
2019-11-20 23:11:13 -0500

    Signed-off-by: Song-Fang <sof10@pitt.edu>

--- proxy/src/main/java/com/innerClass/InnerClass2.java
+++ proxy/src/main/java/com/innerClass/InnerClass2.java
@@ -2,8 +2,11 @@ package com.innerClass;
 
 public class InnerClass2 {
     public static void main(String[] args) {
-
+        TestInnerClass testInnerClass = new TestInnerClass();
+        testInnerClass.display();
     }
+
+
 
     public Comparable getCompare(){
         //匿名接口的有名实现类
@@ -17,9 +20,26 @@ public class InnerClass2 {
         //匿名接口的匿名实现类
         return new Comparable(){
             public int compareTo(Object o){
+
                 return 0;
             }
         };
 
+    }
+}
+
+class TestInnerClass{
+
+    public void display(){
+       final int value = 5;
+
+         class InnerClass{
+             int value = 10;
+            public void display(){
+                System.out.println("value="+value);
+            }
+        }
+
+        new InnerClass().display();
     }
 }
